a4b6f106d8fe6f1ae22f8ad74f42f4adefbd8ab3
nahuel <nahuel_a_94@hotmail.com>
2021-05-03 21:12:38 -0300

    master

---
+++ codigo/ActividadesPermitidas.py
@@ -0,0 +1,8 @@
+class ActividadesPermitidas:
+
+    def __init__(self,actividad):
+        actividad=[]
+        
+        self.actividad=actividad
+
+        actividad.append(self.actividad)
---
+++ codigo/Persona.py
@@ -0,0 +1,9 @@
+class Persona:
+
+    def __init__(self,nom,apell,dni,domi,tel,email,actividad):
+        self.nombre=nom
+        self.apellido=apell
+        self.dni=dni
+        self.domicilio=domi
+        self.email=email
+        self.actividad=actividad
